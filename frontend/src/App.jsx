df9424c68abd945c48c3a6a6bb70bcde4a3d4936
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-05-20 22:40:33 +0530

    pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/signup' element={<Signup />} />
+    <Route path='/login' element={<Login />} />
+   </Routes>
   )
 }
 
